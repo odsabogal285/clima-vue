924024697336ea9046e7e442d8180d4aaa0c44e9
Oscar Sabogal <oscarsabogal285@gmail.com>
2024-02-29 11:55:59 -0500

    fix: change https for http

--- src/composables/useClima.js
+++ src/composables/useClima.js
@@ -12,7 +12,7 @@ export default function useClima () {
         error.value = '';
         try {
 
-            const url = `http://api.openweathermap.org/geo/1.0/direct?q=${ciudad},${pais}&limit=1&appid=${key}`;
+            const url = `https://api.openweathermap.org/geo/1.0/direct?q=${ciudad},${pais}&limit=1&appid=${key}`;
             const {data} = await axios(url);
             const {lat, lon} = data[0];
 
